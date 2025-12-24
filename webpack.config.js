debc34ae3b211fd5daee056b8732857f56824886
kenarsuleyman <suleyman.kenar@kroppa.com>
2025-12-24 17:24:31 +0300

    webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,111 +1,130 @@
 const path = require('path');
-const argv = require('yargs').argv;
 const packageJson = require('./package.json');
 const webpack = require('webpack');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const WrapperPlugin = require('wrapper-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const BANNER = `${packageJson.title} v${packageJson.version} | Copyright (c) 2016-present ${packageJson.author}`;
 
-if (argv.prod) {
-    process.env.NODE_ENV = 'production';
-}
-let PROD = process.env.NODE_ENV === 'production';
+// Exporting as a function allows us to detect --mode production reliably
+module.exports = (env, argv) => {
 
-module.exports = {
-    mode: PROD ? 'production' : 'development',
-    devtool: PROD ? 'source-map' : 'eval-source-map',
-    
-    entry: {
-        'keditor': './src/keditor/index.js',
-        'keditor-components': './src/components/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-        filename: `js/[name].js`,
-        globalObject: `typeof self !== 'undefined' ? self : this`
-    },
-    
-    optimization: {
-        minimizer: [
-            new TerserJSPlugin({
-                sourceMap: true
-            }),
-            new OptimizeCSSAssetsPlugin({
-                cssProcessorOptions: {
-                    map: {
-                        inline: false
-                    }
-                }
+    // Check if we are in production mode
+    const isProd = argv.mode === 'production';
+
+    return {
+        mode: isProd ? 'production' : 'development',
+
+        // 'source-map' creates high-quality .map files for Production
+        // 'eval-source-map' is faster for Development
+        devtool: isProd ? 'source-map' : 'eval-source-map',
+
+        entry: {
+            'keditor': './src/keditor/index.js',
+            'keditor-components': './src/components/index.js'
+        },
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            library: {
+                type: 'umd',
+                name: '[name]',
+                export: 'default',
+            },
+            filename: `js/[name].js`,
+            globalObject: 'this',
+            clean: true,
+        },
+
+        optimization: {
+            minimize: isProd,
+            minimizer: [
+                new TerserPlugin({
+                    extractComments: false,
+                }),
+                new CssMinimizerPlugin(),
+            ],
+        },
+
+        plugins: [
+            new webpack.BannerPlugin(BANNER),
+            new MiniCssExtractPlugin({
+                filename: `css/[name].css`
             })
         ],
-    },
-    
-    plugins: PROD ? [
-        new webpack.BannerPlugin(BANNER),
-        new WrapperPlugin({
-            test: /\.css$/,
-            footer: `/*# sourceMappingURL=[name].css.map */`
-        }),
-        new MiniCssExtractPlugin({
-            filename: `css/[name].css`
-        })
-    ] : [
-        new MiniCssExtractPlugin({
-            filename: `css/[name].css`
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    },
-                    {
-                        loader: 'string-replace-loader',
-                        options: {
-                            multiple: [
-                                {
-                                    search: '@{version}',
-                                    replace: packageJson.version
-                                }
-                            ]
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        },
+                        {
+                            loader: 'string-replace-loader',
+                            options: {
+                                multiple: [
+                                    {
+                                        search: '@{version}',
+                                        replace: packageJson.version
+                                    }
+                                ]
+                            }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'less-loader'
-                ],
+                    ]
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        // CSS Loader must have sourceMap: true
+                        {
+                            loader: 'css-loader',
+                            options: { sourceMap: true }
+                        },
+                        // Sass Loader must have sourceMap: true
+                        {
+                            loader: 'sass-loader',
+                            options: { sourceMap: true }
+                        }
+                    ],
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: { sourceMap: true }
+                        },
+                        {
+                            loader: 'less-loader',
+                            options: { sourceMap: true }
+                        }
+                    ],
+                }
+            ]
+        },
+
+        externals: {
+            jquery: 'jQuery',
+            keditor: 'KEditor',
+            ckeditor: 'CKEDITOR'
+        },
+
+        resolve: {
+            modules: [path.resolve('./node_modules'), path.resolve('./src')],
+            extensions: ['.json', '.js'],
+            fallback: {
+                "path": false,
+                "fs": false
             }
-        ]
-    },
-    externals: {
-        jquery: 'jQuery',
-        keditor: 'KEditor',
-        ckeditor: 'CKEDITOR'
-    },
-    resolve: {
-        modules: [path.resolve('./node_modules'), path.resolve('./src')],
-        extensions: ['.json', '.js']
-    }
+        }
+    };
 };
